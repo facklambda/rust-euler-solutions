384ab6d6183864f8eea5d4a10284cd4c3d6d6bb3
fack <fack@ersatz.systems>
2021-06-23 23:21:00 -0500

    progress on problem 3

--- problem-3/src/main.rs
+++ problem-3/src/main.rs
@@ -1,6 +1,19 @@
-extern crate primal;
-
+use std::f64;
 fn main () {
-    let sieve = primal::Sieve::new(600851475143);
-    println!("{:?}", sieve.factor(600851475143));
+    let n = 600851475143;
+    let maxval = 600851475143.sqrt();
+
+    while n % 2 == 0 {
+        println!("2");
+        n /= 2;
+    }
+
+    for i in 3..maxval {
+        
+        while n % i == 0 {
+            println!("{0}", i);
+            n /= i;
+        }
+
+    }
 }
