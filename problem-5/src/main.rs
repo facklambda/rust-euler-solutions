9cbbc18a2c4336ef5241e961311520701185150b
fack <fack>
2021-07-02 03:54:56 +0000

    fuck it lol

--- problem-5/src/main.rs
+++ problem-5/src/main.rs
@@ -1,18 +1,10 @@
 fn main() {
-    let mut n: u32 = 0;
+    let mut i: u32 = 20;
+    while i % 2 != 0 || i % 3 != 0 || i % 4 != 0 || i % 5 != 0 || i % 6 != 0 || i % 7 != 0 || i % 8 != 0 || i % 9 != 0 || i % 10 != 0 || i % 11 != 0 || i % 12 != 0 || i % 13 != 0 || i % 14 != 0 || i % 15 != 0 || i % 16 != 0 || i % 17 != 0 || i % 18 != 0 || i % 19 != 0 || i % 20 != 0 {
+        println!("lol, {}", i);
+        i += 20;
 
-    for i in 1..=20 {
-        while divvy_func(n, i) == false {
-            n += 20;
-            println!("n is: {}", n)
-        }
-        println!("i is: {}", i)
-        i += 1;
     }
-}
-
-fn divvy_func(x: u32, y: u32) -> bool {
-    x % y != 0
 }
 // 232792560
 // 90716300
